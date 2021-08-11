2198498a8db9eea91066b5a142b11555d89695fe
Melih <melihdizdar99@gmail.com>
2021-08-12 02:37:08 +0300

    aboutme.jsx'e circle image eklendi ve çeşitli hazırlıklar yapıldı

--- src/pages/aboutme/AboutMe.jsx
+++ src/pages/aboutme/AboutMe.jsx
@@ -1,12 +1,18 @@
 import "./aboutme.css"
-/* import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button" */
+import { Image } from "react-bootstrap"
+
 
 export default function AboutMe() {
     return (
         <div className="aboutme">
                 <div className="aboutmeLeft">
-                    left
+                    <div className="aboutmeLeftItem">
+                        <Image src="https://i.hizliresim.com/kyok9pb.png" className="image" roundedCircle />
+                        <div className="lefthead">Melih Dizdar</div>
+                        <div className="bluebar">bluebar</div>
+                        <div className="lefttitle">WEB DEVELOPER</div>
+                        <div className="sociallinks">Social Media</div>
+                    </div>
                 </div>
                 <div className="aboutmeRight">
                     right
