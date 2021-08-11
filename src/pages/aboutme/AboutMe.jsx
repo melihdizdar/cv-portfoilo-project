bfbfe3a6d8bf1a09242ccc51382f117e1b3ea7d9
Melih <melihdizdar99@gmail.com>
2021-08-11 13:23:11 +0300

    aboutme.jsx sayfasındaki bilgilerin duracağı yerin backgroundu ayarlandı ve topbar.css deki hata giderildi

--- src/pages/aboutme/AboutMe.jsx
+++ src/pages/aboutme/AboutMe.jsx
@@ -1,9 +1,16 @@
 import "./aboutme.css"
+/* import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button" */
 
 export default function AboutMe() {
     return (
         <div className="aboutme">
-            aboutme
+                <div className="aboutmeLeft">
+                    left
+                </div>
+                <div className="aboutmeRight">
+                    right
+                </div>
         </div>
     )
 }
