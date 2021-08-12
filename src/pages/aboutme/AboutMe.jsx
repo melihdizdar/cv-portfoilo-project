618fd90dbf54aa341325c4022a2a3193c489fd89
Melih <melihdizdar99@gmail.com>
2021-08-13 01:29:11 +0300

    topbardaki topname kısmındaki font 15px değerine düşürüldü ve aboutme.jsx kısmının sol tarafı bitirildi

--- src/pages/aboutme/AboutMe.jsx
+++ src/pages/aboutme/AboutMe.jsx
@@ -8,10 +8,20 @@ export default function AboutMe() {
                 <div className="aboutmeLeft">
                     <div className="aboutmeLeftItem">
                         <Image src="https://i.hizliresim.com/kyok9pb.png" className="image" roundedCircle />
-                        <div className="lefthead">Melih Dizdar</div>
-                        <div className="bluebar">bluebar</div>
+                        <div className="leftname">Melih</div>
+                        <div className="leftsurname">Dizdar</div>
+                        <div className="bluebar"></div>
                         <div className="lefttitle">WEB DEVELOPER</div>
-                        <div className="sociallinks">Social Media</div>
+                        <div className="sociallinks">
+                            <ul>
+                                <li className="sociallinkslist">
+                                    <i class="fab fa-facebook-f"></i>
+                                    <i class="fab fa-twitter"></i>
+                                    <i class="fab fa-linkedin-in"></i>
+                                    <i class="fab fa-instagram"></i>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div className="aboutmeRight">
