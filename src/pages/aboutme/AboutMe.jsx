3be2ccb5f2dc510127da87c48d87c07150a8c3e7
Melih <melihdizdar99@gmail.com>
2021-08-13 22:32:05 +0300

    botbar.css,aboutme.css ve AboutMe.jsx update
    AboutMe.jsx de responsive çalışmaları yapıldı ve botbar.css'in fixed durumu değiştirildi.

--- src/pages/aboutme/AboutMe.jsx
+++ src/pages/aboutme/AboutMe.jsx
@@ -1,40 +1,44 @@
 import "./aboutme.css"
-import { Image } from "react-bootstrap"
-import { Button } from "react-bootstrap"
-
+import { Button, Card } from "react-bootstrap"
 
 export default function AboutMe() {
     return (
-        <div className="aboutme">
+        <div className="about">
+            <div className="aboutme">
                 <div className="aboutmeLeft">
-                    <div className="aboutmeLeftItem">
-                        <Image src="https://i.hizliresim.com/kyok9pb.png" className="image" roundedCircle />
-                        <div className="leftname">Melih</div>
-                        <div className="leftsurname">Dizdar</div>
-                        <div className="bluebar"></div>
-                        <div className="lefttitle">WEB DEVELOPER</div>
-                        <div className="sociallinks">
-                            <ul>
-                                <li className="sociallinkslist">
-                                    <a href="https://www.facebook.com/melihdizdarr/"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="https://twitter.com/benmelko"><i class="fab fa-twitter"></i></a>
-                                    <a href="https://www.linkedin.com/in/melihdizdar/"><i class="fab fa-linkedin-in"></i></a>
-                                    <a href="https://www.instagram.com/melihdizdarr/"><i class="fab fa-instagram"></i></a>
-                                </li>
-                            </ul>
-                        </div>
+                    <Card className="Card">
+                        <Card.Img variant='top' src="https://i.hizliresim.com/kyok9pb.png" className="image"/>
+                        <Card.Body>
+                            <Card.Title className="leftname">Melih</Card.Title>
+                            <Card.Title className="leftsurname">Dizdar</Card.Title>
+                            <div className="bluebar"></div>
+                            <Card.Title className="lefttitle">WEB DEVELOPER</Card.Title>
+                        </Card.Body>
+                    </Card>
+                    <div className="sociallinks">
+                        <ul>
+                            <li className="sociallinkslist">
+                                <a href="https://www.facebook.com/melihdizdarr/"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com/benmelko"><i class="fab fa-twitter"></i></a>
+                                <a href="https://www.linkedin.com/in/melihdizdar/"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="https://www.instagram.com/melihdizdarr/"><i class="fab fa-instagram"></i></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <div className="aboutmeRight">
-                    <div className="rightHeader">Hello</div>
-                    <div className="rightTitle">Here's who I am and what I do</div>
-                    <div className="rightButtons">
-                        <Button variant="primary" size="default" className="button">Resume</Button>&nbsp;
-                        <Button variant="primary" size="default" className="button">Projects</Button>
+                    <div className="rightItem">
+                        <div className="rightHeader">Hello</div>
+                        <div className="rightTitle">Here's who I am and what I do</div>
+                        <div className="rightButtons">
+                            <Button variant="primary" size="default" className="button">Resume</Button>&nbsp;
+                            <Button variant="primary" size="default" className="button">Projects</Button>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec odio eu elit porttitor finibus. Aliquam pellentesque massa sed libero placerat eleifend.</p>
+                        <p>Nam mollis enim neque, nec fermentum mauris consectetur non. Suspendisse efficitur dictum odio. Ut posuere risus nunc, in bibendum.</p>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec odio eu elit porttitor finibus. Aliquam pellentesque massa sed libero placerat eleifend.</p>
-                    <p>Nam mollis enim neque, nec fermentum mauris consectetur non. Suspendisse efficitur dictum odio. Ut posuere risus nunc, in bibendum.</p>
                 </div>
+            </div>
         </div>
     )
 }
